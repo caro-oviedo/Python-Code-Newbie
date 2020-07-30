93fd514e6658da245115a7b438a6edcac6784f94
caro-oviedo <carolina.u.oviedo@gmail.com>
2020-07-30 20:30:57 -0300

    Completed count_words

--- working_with_files/count_words.py
+++ working_with_files/count_words.py
@@ -1,1 +1,15 @@
+def count_word (filename, word):
+  try: 
+    with open(filename) as f: 
+      contents = f.read()
+  except FileNotFoundError:
+      pass
+  else:
+      count = contents.lower().count(word)
+      print(f"The word {word} appears {count} times in {filename}")
+  
+filenames = ['alice_in_wonderland', 's_h']
+word = input("What word do you want to count? ")
 
+for filename in filenames:
+  count_word(filename, word)
